cf17d04ea040846a723eb3b6182b639b9c4486fc
a2748272054 <130366232+a2748272054@users.noreply.github.com>
2025-09-18 12:34:20 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { R2Explorer } from "r2-explorer";
 export default R2Explorer({
   readonly: false,
   basicAuth: {
-    username: process.env.BASIC_USER,
-    password: process.env.BASIC_PASS
+    username: BASIC_USER,  // 直接用绑定变量名
+    password: BASIC_PASS
   }
 });
